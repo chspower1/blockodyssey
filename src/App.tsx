a020bd0b5fc9a7085e269f0fbc4a0b6f4a1d7092
chspower1 <chspower1@gmail.com>
2022-12-28 17:21:04 +0900

    feat(header) : header html 구조

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <div>
+      <header>
+        상품 검색
+        <div />
+        <div>
+          <div>검색</div>
+          <select name="select">
+            <option value="title">상품명</option>
+            <option value="brand">브랜드</option>
+            <option value="description">상품내용</option>
+          </select>
+          <input type="text" />
+          <button>조회</button>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;

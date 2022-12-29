376db901f5ae96701a2035a406387131427df732
chspower1 <chspower1@gmail.com>
2022-12-29 10:27:35 +0900

    feat(pagination) : section 구분

--- src/components/contents/ProductList.tsx
+++ src/components/contents/ProductList.tsx
@@ -13,8 +13,8 @@ interface ProductListProps {
 const ProductList = ({ searchOptions: { search, category } }: ProductListProps) => {
   const [postPerPage, setPostPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
+  const [pageSection, setPageSection] = useState(1);
   const [resultProducts, setResultProducts] = useState<ResultProducts>();
-
   const prevProductsCount = postPerPage * (currentPage - 1);
   const currentProductsCount = postPerPage * currentPage;
   const maxPage = resultProducts ? Math.ceil(resultProducts.total / postPerPage) : 1;
@@ -24,9 +24,20 @@ const ProductList = ({ searchOptions: { search, category } }: ProductListProps) 
   const handleClickPageButton = (page: number) => {
     setCurrentPage(page);
   };
+
   const handleChangePostPerPage = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setPostPerPage(parseInt(e.currentTarget.value));
   };
+
+  const handleClickPageSection = ({ isNext }: { isNext: boolean }) => {
+    if (isNext && pageSection * 5 <= maxPage) {
+      setPageSection((prev) => prev + 1);
+    } else if (!isNext && pageSection !== 1) {
+      setPageSection((prev) => prev - 1);
+    }
+  };
+
+  // 검색조건,키워드 변경시
   useEffect(() => {
     setCurrentPage(1);
     if (products) {
@@ -63,15 +74,33 @@ const ProductList = ({ searchOptions: { search, category } }: ProductListProps) 
         </select>
         {resultProducts && (
           <div>
-            {Array.from({ length: maxPage }, (_, index) => index + 1).map((page) => (
-              <button
-                key={page}
-                style={currentPage === page ? { backgroundColor: "red" } : {}}
-                onClick={() => handleClickPageButton(page)}
-              >
-                {page}
-              </button>
-            ))}
+            <button
+              onClick={() => handleClickPageSection({ isNext: false })}
+              disabled={pageSection === 1}
+            >
+              ⬅️
+            </button>
+            {Array.from({ length: maxPage }, (_, index) => index + 1).map(
+              (page) =>
+                ((page <= 5 * pageSection && page > 5 * (pageSection - 1)) || page === maxPage) && (
+                  <>
+                    {page === maxPage && "..."}
+                    <button
+                      key={page}
+                      style={currentPage === page ? { backgroundColor: "red" } : {}}
+                      onClick={() => handleClickPageButton(page)}
+                    >
+                      {page}
+                    </button>
+                  </>
+                )
+            )}
+            <button
+              onClick={() => handleClickPageSection({ isNext: true })}
+              disabled={pageSection * 5 >= maxPage}
+            >
+              ➡️
+            </button>
           </div>
         )}
       </div>
